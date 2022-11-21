626a0f0c74fd8c4c45ebcaa513411290ca2d4457
vladissta <staninvladislav@mail.ru>
2022-11-22 01:01:50 +0300

    Project ver. 2

--- Project/Reports/report.py
+++ Project/Reports/report.py
@@ -6,16 +6,14 @@
 
 # ### Importing requirements
 
-# In[75]:
+# In[1]:
 
 
-import numpy as np
 import pandas as pd
 import glob
 import matplotlib.pyplot as plt
 import seaborn as sns
 import scipy.stats as stats
-import statsmodels.api as sm
 
 
 # ### Function that rquires path, file extension and separator to parse all files in path and concate them all in one dataframe
@@ -79,9 +77,9 @@ for i in ['Sex', 'Team', 'Games', 'Season', 'City', 'Sport', 'Medal']:
 df.isna().sum()
 
 
-# _Obviously some sportsmen did not have medals. Also we consider that some athletes did not measured their Height and Weight and did not wanted to say their age (maybe)_
+# _Obviously some sportsmen did not have medals. Also we consider that some athletes did not measured their Height and Weight and did not wanted to say their age (maybe). And nobody took such statistics especially in the several first games_
 
-# #### Deleting sportsmen without name and sport, beacuse sportsmen could not be without sport 
+# #### Deleting sportsmen without name and sport, beacuse sportsmen could not be without sport and name
 
 # In[9]:
 
@@ -159,39 +157,53 @@ df['Age'].value_counts().sort_index()
 df.loc[df.Age == 240]
 
 
-# _Too old... We have to change this data to NULL_
+# _Too old... We have to change this data to 24, beacuse she have some another data in dataset - he was 24 in 1912_
 
 # In[20]:
 
 
-df.loc[df.Age == 240, 'Age'] = None
+df.loc[(df.Name == "Flicien Jules mile Courbet") & (df.Year == 1912)].Age
+
+
+# In[21]:
+
+
+df.loc[df.Age == 240, 'Age'] = 24
 
 
 # #### Checking Height
 
-# In[21]:
+# In[22]:
 
 
 df['Height'].value_counts().sort_index()
 
 
-# In[22]:
+# In[23]:
 
 
 df.loc[df.Height == 340]
 
 
-# _It is to high!_
+# _It is to high! But let's take a look to another data about his height_
 
-# In[23]:
+# In[24]:
 
 
-df.loc[df.Height == 340, 'Height'] = None
+df.loc[df.Name == 'Kirsty Leigh Coventry (-Seward)'].Height.value_counts()
+
+
+# She is definetely 176!
+
+# In[25]:
+
+
+df.loc[df.Height == 340, 'Height'] = 176
 
 
 # #### Checking Weight
 
-# In[24]:
+# In[26]:
 
 
 df['Weight'].value_counts().sort_index()
@@ -203,7 +215,7 @@ df['Weight'].value_counts().sort_index()
 
 # ### The age of the youngest athletes of both sexes at the 1992 Olympics.
 
-# In[25]:
+# In[27]:
 
 
 df.loc[df.Year == 1992].groupby('Sex').Age.min().to_frame()
@@ -211,7 +223,7 @@ df.loc[df.Year == 1992].groupby('Sex').Age.min().to_frame()
 
 # ### The average value and standard deviation of the Height variable for athletes of each sex.
 
-# In[26]:
+# In[28]:
 
 
 df.groupby('Sex').agg(Mean_value = ('Height', 'mean'),
@@ -220,7 +232,7 @@ df.groupby('Sex').agg(Mean_value = ('Height', 'mean'),
 
 # ### The average value and standard deviation of the Height variable for female tennis players at the 2000 Olympics.
 
-# In[27]:
+# In[29]:
 
 
 df.loc[(df.Sex == 'F') & 
@@ -231,7 +243,7 @@ df.loc[(df.Sex == 'F') &
 
 # ### Heaviest athlete's sport in at the 2006 Olympics
 
-# In[200]:
+# In[30]:
 
 
 print(f"Sport of heviest athlete: {(df.loc[(df.Weight == df.loc[df.Year == 2006].Weight.max()) & (df.Year == 2006)].Sport).reset_index().Sport[0]}")
@@ -239,7 +251,7 @@ print(f"Sport of heviest athlete: {(df.loc[(df.Weight == df.loc[df.Year == 2006]
 
 # ### Number of gold medals which were received by women from 1980 to 2010
 
-# In[29]:
+# In[31]:
 
 
 df.loc[(df.Sex == "F") & (df.Medal == "Gold") & (df.Year.isin(range(1980,2011)))].Medal.count()
@@ -247,7 +259,7 @@ df.loc[(df.Sex == "F") & (df.Medal == "Gold") & (df.Year.isin(range(1980,2011)))
 
 # ### Number of times has John Aalberg participated in the Olympic Games in different years
 
-# In[30]:
+# In[32]:
 
 
 diff_years = df.loc[df.Name == "John Aalberg"].Year.unique().size
@@ -259,7 +271,7 @@ print(f'Different years he participated: {diff_years}. Participated at different
 # ### The least and most represented (by number of participants) age groups of athletes at the 2008 Olympics. 
 # ##### Age groups: [15-25), [25-35), [35-45), [45-55].
 
-# In[31]:
+# In[33]:
 
 
 categories = pd.cut(df.loc[df.Year == 2008].Age,bins=(15,25,35,45,55), right= False)
@@ -272,7 +284,7 @@ ages_df
 
 # ### How much has the number of sports at the 2002 Olympics more compared to the 1994 Olympic Games
 
-# In[182]:
+# In[34]:
 
 
 print(f" In 2002 Olympics there were {df.loc[df.Year == 2002].Sport.unique().size - df.loc[df.Year == 1994].Sport.unique().size} more sports then in 1994 Olympics")
@@ -280,33 +292,40 @@ print(f" In 2002 Olympics there were {df.loc[df.Year == 2002].Sport.unique().siz
 
 # ### The top 3 countries for each type of medals for the Winter and Summer Olympics
 
-# In[33]:
+# In[35]:
 
 
 medal_top_df = df.groupby(['Season', "Medal", "NOC"]).NOC.count().sort_values(ascending=False).\
-groupby(level=["Medal", "Season"]).head(3).sort_index(level=[0,1]).\
-reindex(['Gold', 'Silver', "Bronze"], level=1).to_frame().rename(columns = {'NOC': 'Count'})
+groupby(level=["Medal", "Season"]).head(3).reindex(['Gold', 'Silver', "Bronze"], level=1).to_frame().\
+rename(columns = {'NOC': 'Count'})
 medal_top_df
 
 
 # ###  Height_z_scores variable with the values of the Height variable after its standardization
 
-# In[34]:
+# In[76]:
 
 
-Height_z_scores = stats.zscore(df.Height.dropna())
-Height_z_scores
+df['Height_z_scores'] = (df.Height - df.Height.mean()) / df.Height.std()
+df.Height_z_scores
 
 
 # ### Height_min_max_scaled variable with the values of the Height variable after applying min-max normalization to it.
 # ##### Optional
+
+# In[77]:
+
+
+df['Height_min_max_scaled'] = (df.Height - df.Height.min()) / (df.Height.max() - df.Height.min())
+df['Height_min_max_scaled']
+
 
 # ### Compared the height, weight and age of men and women who participated in the Winter Olympic Games. 
 # ##### The results designed to use them for the article.
 
 # _As we have huge data (big amount of values), t-test could be applied_
 
-# In[148]:
+# In[78]:
 
 
 t_height = stats.ttest_ind(df.loc[df.Sex == 'F', 'Height'].dropna(),
@@ -317,7 +336,7 @@ t_age = stats.ttest_ind(df.loc[df.Sex == 'F', 'Age'].dropna(),
 df.loc[df.Sex == 'M', 'Age'].dropna())
 
 
-# In[160]:
+# In[79]:
 
 
 new_df = df.loc[df.Season == 'Winter',['Sex','Height','Weight', 'Age']].groupby('Sex', as_index=False).agg(['min','mean', 'max', 'std', "count"]).round(2).transpose()
@@ -341,7 +360,7 @@ new_df.loc[('Weight', 'Maximum value'), 'T-test'] = f'p-value = {t_weight[1].rou
 new_df.loc[('Age', 'Maximum value'), 'T-test'] = f'p-valuec = {t_age[1].round(2)}'
 
 
-# In[161]:
+# In[80]:
 
 
 s1 = new_df.style.format(formatter={'Female': "{:.1f}", 'Male': "{:.1f}", 'T-test p-value': "{:.5f}"})
@@ -363,7 +382,7 @@ s1
 
 # ##### Making tables for article
 
-# In[162]:
+# In[81]:
 
 
 print(s1.to_latex(), file = open('../Tables for article/latex_table_with_style.txt', 'w'))
@@ -375,138 +394,79 @@ print(new_df.to_markdown(), file = open('../Tables for article/markdown_table.tx
 
 # _Making top Teams with the most number of medals of all time_
 
-# In[39]:
+# In[82]:
 
 
-
-
-
-# In[40]:
-
-
-medals = df.loc[df.Medal.notna()].groupby('NOC').Medal.count().reset_index().sort_values(by='Medal', ascending=False)
-num_medals = df.loc[df.Medal == "Gold"].groupby('NOC').Medal.count().reset_index().\
+medals = df.loc[df.Medal.notna()].groupby('Team').Medal.count().reset_index().sort_values(by='Medal', ascending=False)
+num_medals = df.loc[df.Medal == "Gold"].groupby('Team').Medal.count().reset_index().\
 sort_values(by='Medal', ascending=False).rename(columns = {'Medal': 'Number of medals'})
 num_medals.head(10)
 
 
-# #### _Normalizing to number of unique events these countries participated_
+# We already can assume that some Teams have more medals then others! But it is difficult to make some correlation. Let's assume that Team and Medal variables connected via Sports variable (in particular - the number of kinds of sports Team is participated in)
 
-# In[41]:
+# #### How many kinds of sports Teams is participated in?
+
+# In[83]:
 
 
-num_sport = df.groupby('NOC').Sport.nunique()
-num_sport_medals = num_medals.merge(num_sport, on='NOC').rename(columns={'Sport':'Number of sports participated'})
+num_sport = df.groupby('Team').Sport.nunique()
+num_sport_medals = num_medals.merge(num_sport, on='Team').rename(columns={'Sport':'Number of sports participated'})
 num_sport_medals
 
 
-# In[42]:
+# In[84]:
 
 
-num_sport_medals['Medal per sport'] = num_sport_medals['Number of medals']/num_sport_medals['Number of sports participated']
+sns.scatterplot(x=num_sport_medals['Number of medals'], y=num_sport_medals['Number of sports participated']);
 
 
-# In[43]:
+# _It seemas like it is better to use spearman test (nonlinear, non-homoscedastic etc.)_
+
+# In[85]:
 
 
-num_sport_medals = num_sport_medals.sort_values(by = 'Number of medals', ascending=False)
-num_sport_medals
+print(f"Spearman's r is {stats.spearmanr(num_sport_medals['Number of medals'], num_sport_medals['Number of sports participated']).correlation}")
 
 
-# ##### Checking if we can make linear model
+# ##### Let's see some additional plots !
 
-# In[44]:
-
-
-x=num_sport_medals['Number of medals']
-y=num_sport_medals['Number of sports participated']
-
-x = sm.add_constant(x)
+# In[86]:
 
 
-# _Deleting deviations accroding to cook's distances_
-
-# In[45]:
-
-
-model = sm.OLS(y, x).fit()
-
-
-# In[46]:
-
-
-cooks = model.get_influence().cooks_distance
-
-
-# In[47]:
-
-
-df_wo_devs = num_sport_medals[cooks[1] > 0.05]
-
-
-# _Checking Homoscedacity using barlett and levene tests_
-
-# In[179]:
-
-
-print('p-value for levene test is')
-levene(df_wo_devs['Number of medals'],
-df_wo_devs['Number of sports participated']).pvalue
-
-
-# In[180]:
-
-
-print('p-value for bartlett test is')
-bartlett(df_wo_devs['Number of medals'],
-df_wo_devs['Number of sports participated']).pvalue
-
-
-# _Homoscedacity condition have not been approved_
-
-# In[50]:
-
-
-sns.scatterplot(x=df_wo_devs['Number of medals'], y=df_wo_devs['Number of sports participated']);
-
-
-# ##### Let's see the plots
-
-# In[52]:
-
-
-fig, ax = plt.subplots(1, 3, figsize = (14, 4))
-sns.barplot(data=num_sport_medals, x='NOC', y='Medal per sport', ax = ax[0]);
+fig, ax = plt.subplots(1, 2, figsize = (14, 4))
+sns.barplot(data=num_sport_medals, x='Team', y='Number of medals', ax = ax[0]);
 ax[0].set(xticklabels=[]);
 ax[0].tick_params(axis='y', which='major', labelsize=7)
-sns.barplot(data=num_sport_medals, x='NOC', y='Number of medals', ax = ax[1]);
+sns.barplot(data=num_sport_medals, x='Team', y='Number of sports participated', ax = ax[1]);
 ax[1].set(xticklabels=[]);
 ax[1].tick_params(axis='y', which='major', labelsize=7)
-sns.barplot(data=num_sport_medals, x='NOC', y='Number of sports participated', ax = ax[2]);
-ax[2].set(xticklabels=[]);
-ax[2].tick_params(axis='y', which='major', labelsize=7)
 
 
-# We could not use correlation test, therefore we made 3 plots. All NOCs on plots sorted by Number of medals (you can see it on 2nd plot).  
+# All Teams on plots sorted by Number of medals (you can see it on 1st plot).  
 # 
-# As seen on 1st plot, it is a little bit similar to 2nd. However some NOCs have numbers of medals significantly greater then  number of sports they participated -> These NOCs are professionals in their sports. (It is peaks on 1st plot and gaps on 3rd plot)  
+# Some Teams have numbers of medals significantly greater then  number of sports they participated -> These Teams are professionals in their sports or it is particular sport team!. (It is gaps on 2nd plot)  
 # 
-# Low values of Medal per sport variable (on 1st plot) belong to some NOCs with high number of sports they participated (3rd plot). These NOCs tries a lot of different sports, but still have small number of medals.  
+# Some Teams with high number of sports they participated (2nd plot - high values) have low number of medals. These Teams tries a lot of different sports, but still have small number of medals.  
 # 
-# In common we can conclude that number of sports in which NOC participate could affect the number of medals they have. But there are some deviations, where some NOCs are professionals in their small amount of sports and NOCs that could not find they successufull sport  
+# In common we can conclude that number of sports in which Team participate affects the number of medals they have. But there are some deviations, where some Teams are professionals in their small amount of sports (or one kind of sport) and Teams that could not find they successufull sport
+# 
+# That is why correlation is not equal to 1!
+# 
+# **So Team and Medal variables is connected. Particular Teams partcicpate in many kind of sports or even in only one and then recieve many or few medals!**
 
 # ## Some additional hypothesis
 
 # ### Is the average number of medals in Women and Men significant?
 
-# In[69]:
+# In[87]:
 
 
 m_medals = df.loc[(df.Sex == 'M') & (df.Medal.notnull())].groupby('Name').Medal.count()
 f_medals = df.loc[(df.Sex == 'F') & (df.Medal.notnull())].groupby('Name').Medal.count()
 
 
-# In[70]:
+# In[88]:
 
 
 f_medals.value_counts()
@@ -514,13 +474,13 @@ f_medals.value_counts()
 
 # _Distribution is not normal, but number of values is still big_
 
-# In[71]:
+# In[89]:
 
 
 m_medals.describe()
 
 
-# In[72]:
+# In[90]:
 
 
 f_medals.describe()
@@ -528,33 +488,31 @@ f_medals.describe()
 
 # _It is better to use another test (Mann-Whitney) since distribution significantly not normal_
 
-# In[177]:
+# In[51]:
 
 
-mann = mannwhitneyu(m_medals, f_medals)
+mann = stats.mannwhitneyu(m_medals, f_medals)
 print(f'p-value is {mann.pvalue}')
 
 
-# **Differences still significant.**
-
-# _I am not sexist. It were the most common tests that i vave in my mind with comparison of 2 groups_
+# _**Differences still significant.**_
 
 # ### Is weights of swimmers is significantly differ from footballer's?
 
-# In[170]:
+# In[52]:
 
 
 footbalers_weights = df.loc[(df.Sport == 'Football')].Weight.dropna()
 swimmers_weights = df.loc[(df.Sport == 'Swimming')].Weight.dropna()
 
 
-# In[172]:
+# In[53]:
 
 
 footbalers_weights.describe()
 
 
-# In[173]:
+# In[54]:
 
 
 swimmers_weights.describe()
@@ -562,16 +520,40 @@ swimmers_weights.describe()
 
 # _There are almost no differences yet!_
 
-# In[202]:
+# In[55]:
 
 
-print(f"p-value = {ttest_ind(swimmers_weights, footbalers_weights).pvalue}")
+print(f"p-value = {stats.ttest_ind(swimmers_weights, footbalers_weights).pvalue}")
 
 
 # _They have the same weight! Is that mean that footballers would not drown..?_
 
-# In[ ]:
+# ### Is it true that over time more Teams have become involved in games?
+
+# In[56]:
 
 
+num_of_nocs = df.loc[df.Season == 'Summer'].groupby('Year').Team.nunique().reset_index()
 
 
+# In[57]:
+
+
+sns.scatterplot(data=num_of_nocs, x = 'Year', y="Team");
+
+
+# In[58]:
+
+
+print(f" Test of homoscedacity (p-value): {stats.levene(num_of_nocs.Year, num_of_nocs.Team).pvalue}")
+
+
+# It is better to use Spearman's test
+
+# In[59]:
+
+
+print(f"Spearman's r is {stats.spearmanr(num_of_nocs.Year, num_of_nocs.Team).correlation}")
+
+
+# _More teams each year!_
